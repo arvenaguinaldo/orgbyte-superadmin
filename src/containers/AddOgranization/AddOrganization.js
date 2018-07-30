bc8e44a9516cabd5a348fcc707b1796e99ed0668
Arven Aguinaldo <arvencaguinaldo@gmail.com>
2018-07-31 01:59:25 +0800

    Add textfields

--- src/containers/AddOgranization/AddOrganization.js
+++ src/containers/AddOgranization/AddOrganization.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {withStyles} from '@material-ui/core/styles';
 import Stepper from '@material-ui/core/Stepper';
@@ -7,7 +7,21 @@ import StepLabel from '@material-ui/core/StepLabel';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
+import TextField from '@material-ui/core/TextField';
+import MaskedInput from 'react-text-mask';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+
+
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+// import FormControlLabel from '@material-ui/core/FormControlLabel';
+// import FormLabel from '@material-ui/core/FormLabel';
+// import RadioGroup from '@material-ui/core/RadioGroup';
+// import Radio from '@material-ui/core/Radio';
 
 // layout
 import LayoutWithTopbarAndSidebar from 'layouts/LayoutWithTopbarAndSidebar';
@@ -19,6 +33,13 @@ const styles = theme => ({
   root: {
     width: '100%'
   },
+  Paper: {
+    padding: '50px'
+  },
+  textField: {
+    // marginLeft: theme.spacing.unit,
+    // marginRight: theme.spacing.unit
+  },
   backButton: {
     marginRight: theme.spacing.unit
   },
@@ -26,36 +47,203 @@ const styles = theme => ({
     marginTop: theme.spacing.unit,
     marginBottom: theme.spacing.unit
   },
+  grid: {
+    backgroundColor: '#5F1D24'
+  },
   hide: {
     display: 'none'
   }
 });
+/* eslint-disable react/prefer-stateless-function */
 
-function getSteps() {
-  return ['Organization Profile', 'President Profile', 'Create an ad'];
+function TextMaskCustom(props) {
+  const {inputRef, ...other} = props;
+
+  return (
+    <MaskedInput
+      {...other}
+      ref={inputRef}
+      mask={[/\d/, /\d/, '-', /\d/, /\d/, /\d/]}
+      placeholderChar={'\u2000'}
+      showMask
+    />
+  );
 }
 
-function getStepContent(stepIndex) {
-  switch (stepIndex) {
-    case 0:
-      return 'Organization Profile';
-    case 1:
-      return 'President Profile';
-    case 2:
-      return 'This is the bit I really care about!';
-    default:
-      return 'Uknown stepIndex';
-  }
-}
+TextMaskCustom.propTypes = {
+  inputRef: PropTypes.func.isRequired
+};
 
-class AddOrganization extends React.Component {
+
+class AddOrganization extends Component {
   static propTypes = {
     classes: PropTypes.object
   };
 
   state = {
-    activeStep: 0
+    activeStep: 0,
+    textmask: '  -   ',
+    college: '',
+    organizationType: '',
+    error: ''
   };
+
+  getSteps = () => {
+    return ['Organization Profile', 'President Profile', 'Create an ad'];
+  }
+
+  getStepContent = (stepIndex) => {
+    switch (stepIndex) {
+      case 0:
+        return this.orgnazationProfile();
+      case 1:
+        return this.presidentProfile();
+      case 2:
+        return 'This is the bit I really care about!';
+      default:
+        return 'Uknown stepIndex';
+    }
+  }
+
+  handleChange = name => (event) => {
+    this.setState({
+      [name]: event.target.value
+    });
+  };
+
+  orgnazationProfile = () => {
+    const {
+      textmask,
+      college,
+      error
+    } = this.state;
+    const {classes} = this.props;
+    return (
+      <Paper className={style.form} elevation={0} square={false} >
+        <Grid container spacing={24}>
+          <Grid item xs={12}>
+
+            <Grid container spacing={40}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  id="with-placeholder"
+                  label="Name of Organization"
+                  placeholder="Organization Name"
+                  className={classes.textField}
+                  margin="normal"
+                  error={!!error}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <FormControl className={classes.formControl} margin="normal" fullWidth>
+                  <InputLabel htmlFor="age-simple">College</InputLabel>
+                  <Select
+                    value={college}
+                    className={classes.textField}
+                    onChange={this.handleChange}
+                    inputProps={{
+                      name: 'college',
+                      id: 'age-simple'
+                    }}
+                  >
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>College of Information and Communications Technology</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+            </Grid>
+
+            <Grid container spacing={32}>
+              <Grid item xs={6} sm={2}>
+                <TextField
+                  id="with-placeholder"
+                  label="Acronym"
+                  placeholder="Acronym"
+                  className={classes.textField}
+                  margin="normal"
+                />
+              </Grid>
+
+              <Grid item xs={6} sm={2}>
+                <FormControl className={classes.formControl} margin="normal">
+                  <InputLabel htmlFor="formatted-text-mask-input">Recognition Number</InputLabel>
+                  <Input
+                    value={textmask}
+                    onChange={this.handleChange('textmask')}
+                    id="formatted-text-mask-input"
+                    inputComponent={TextMaskCustom}
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={6} sm={2}>
+                <FormControl className={classes.formControl} margin="normal" >
+                  <TextField
+                    id="date"
+                    label="Date of Formation"
+                    type="date"
+                    defaultValue="2017-05-24"
+                    className={classes.textField}
+                    InputLabelProps={{
+                      shrink: true
+                    }}
+                    fullWidth
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={6} sm={3}>
+                <FormControl className={classes.formControl} margin="normal" fullWidth>
+                  <InputLabel htmlFor="age-simple">Nature of Organization</InputLabel>
+                  <Select
+                    value={college}
+                    className={classes.textField}
+                    onChange={this.handleChange}
+                    inputProps={{
+                      name: 'college',
+                      id: 'age-simple'
+                    }}
+                  >
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Academic</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid><Grid item xs={6} sm={3}>
+                <FormControl className={classes.formControl} margin="normal" fullWidth>
+                  <InputLabel htmlFor="age-simple">Type of Organization</InputLabel>
+                  <Select
+                    value={college}
+                    className={classes.textField}
+                    onChange={this.handleChange}
+                    inputProps={{
+                      name: 'college',
+                      id: 'age-simple'
+                    }}
+                  >
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>University Based</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
+    );
+  }
+
+  presidentProfile = () => {
+    return (
+      <Paper className={style.form} elevation={0} square={false} >
+        <p>President Profile</p>
+      </Paper>
+    );
+  }
 
   handleNext = () => {
     const {activeStep} = this.state;
@@ -77,10 +265,11 @@ class AddOrganization extends React.Component {
     });
   };
 
+
   render() {
-    const {classes} = this.props;
-    const steps = getSteps();
     const {activeStep} = this.state;
+    const {classes} = this.props;
+    const steps = this.getSteps();
 
     return (
       <LayoutWithTopbarAndSidebar>
@@ -95,9 +284,9 @@ class AddOrganization extends React.Component {
                 );
               })}
             </Stepper>
-            <Paper className={style.form} elevation={0} square={false} >
-              <h1>asd</h1>
-            </Paper>
+
+            {this.getStepContent(activeStep)}
+
             <div className={style.bottomButton}>
               {this.state.activeStep === steps.length ? (
                 <div>
@@ -108,7 +297,6 @@ class AddOrganization extends React.Component {
                 </div>
               ) : (
                 <div>
-                  <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
                   <div>
                     <Button
                       disabled={activeStep === 0}
