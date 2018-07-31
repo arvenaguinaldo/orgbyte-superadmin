6b1c3f4abc390806dad356e263e6fe4218afe71d
Arven Aguinaldo <arvencaguinaldo@gmail.com>
2018-07-31 16:05:10 +0800

    Change sidebar to fix position

--- src/layouts/sections/Topbar/Topbar.js
+++ src/layouts/sections/Topbar/Topbar.js
@@ -1,70 +1,35 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {withStyles} from '@material-ui/core/styles';
-import classNames from 'classnames';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import Typography from '@material-ui/core/Typography';
 
 
-const drawerWidth = 240;
+const drawerWidth = 250;
 
-const styles = theme => ({
+const styles = () => ({
   appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    }),
-    backgroundColor: '#373737'
-  },
-  appBarShift: {
-    marginLeft: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen
-    })
-  },
-  menuButton: {
-    marginLeft: 12,
-    marginRight: 36
-  },
-  hide: {
-    display: 'none'
+    backgroundColor: '#363736'
   }
 });
 
 class Topbar extends Component {
   static propTypes = {
-    classes: PropTypes.object.isRequired,
-    open: PropTypes.bool.isRequired,
-    onRequestSidebarOpen: PropTypes.func.isRequired
+    classes: PropTypes.object.isRequired
   };
 
   render() {
-    const {
-      classes,
-      open,
-      onRequestSidebarOpen
-    } = this.props;
+    const {classes} = this.props;
 
     return (
       <AppBar
         position="absolute"
-        className={classNames(classes.appBar, open && classes.appBarShift)}
+        className={classes.appBar}
+        color="secondary"
       >
-        <Toolbar disableGutters={!open}>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={onRequestSidebarOpen}
-            className={classNames(classes.menuButton, open && classes.hide)}
-          >
-            <MenuIcon />
-          </IconButton>
+        <Toolbar>
           <Typography variant="title" color="inherit" noWrap>
             SUPER ADMIN
           </Typography>
