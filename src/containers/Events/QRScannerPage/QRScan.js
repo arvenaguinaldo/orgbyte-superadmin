da608373dabfd01e824021e42d8cbfea420b58a2
Lara Beatrice Hilario <larabeatricehilario@gmail.com>
2018-10-18 23:35:33 +0800

    Add qr scanner page

--- src/containers/Events/QRScannerPage/QRScan.js
+++ src/containers/Events/QRScannerPage/QRScan.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import QrReader from 'react-qr-reader';
 // import PropTypes from 'prop-types';
 
+import Center from 'react-center';
+
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
@@ -37,76 +39,78 @@ class Home extends Component {
     return (
       <LayoutWithTopbarAndSidebar>
         <Typography variant="h4" color="secondary"> QR Scanner</Typography>
-        <Paper className={styles.Paper}>
-          <Grid container spacing={0}>
-            <Grid item xs={12} sm={12} md={12}>
-              <Grid container spacing={40}>
-                <Grid item xs={6} sm={6} md={6}>
-                  <QrReader
-                    delay={this.state.delay}
-                    onError={this.handleError}
-                    onScan={this.handleScan}
-                    className={styles.Scanner}
-                    resolution={1000}
-                  />
-                </Grid>
-                <Grid item xs={6} sm={6} md={6}>
-                  <div className={styles.detailsDiv}>
-                    <Typography variant="h4" color="secondary">Attendee Details</Typography>
-                    <List>
-                      <ListItem className={styles.list}>
-                        <Typography variant="h6" color="secondary" className={styles.listTitle}>Name:</Typography>
-                        <ListItemText primary={<Typography variant="h6" color="secondary">Robles, Jermeiah B.</Typography>} />
-                      </ListItem>
+        <Center>
+          <Paper className={styles.Paper}>
+            <Grid container spacing={0}>
+              <Grid item xs={12} sm={12} md={12}>
+                <Grid container spacing={40}>
+                  <Grid item xs={12} sm={12} md={6}>
+                    <QrReader
+                      delay={this.state.delay}
+                      onError={this.handleError}
+                      onScan={this.handleScan}
+                      className={styles.Scanner}
+                      resolution={1000}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={12} md={6}>
+                    <div className={styles.detailsDiv}>
+                      <Typography variant="h4" color="secondary">Attendee Details</Typography>
+                      <List>
+                        <ListItem className={styles.list}>
+                          <Typography variant="h5" color="secondary" className={styles.listTitle}>Name:</Typography>
+                          <ListItemText primary={<Typography variant="h6" color="secondary">Robles, Jermeiah B.</Typography>} />
+                        </ListItem>
 
-                      <ListItem className={styles.list}>
-                        <Typography variant="body1" color="secondary" className={styles.listTitle}>Student Number:</Typography>
-                        <ListItemText primary={<Typography variant="body1" color="secondary">2014-120436</Typography>} />
-                      </ListItem>
+                        <ListItem className={styles.list}>
+                          <Typography variant="h6" color="secondary" className={styles.listTitle}>Student Number:</Typography>
+                          <ListItemText primary={<Typography variant="h6" color="secondary">2014-120436</Typography>} />
+                        </ListItem>
 
-                      <ListItem className={styles.list}>
-                        <Typography variant="body1" color="secondary" className={styles.listTitle}>College:</Typography>
+                        <ListItem className={styles.list}>
+                          <Typography variant="h6" color="secondary" className={styles.listTitle}>College:</Typography>
 
-                        <ListItemText primary={<Typography variant="body1" color="secondary">CICT</Typography>} />
-                      </ListItem>
+                          <ListItemText primary={<Typography variant="h6" color="secondary">CICT</Typography>} />
+                        </ListItem>
 
-                      <ListItem className={styles.list}>
-                        <Typography variant="body1" color="secondary" className={styles.listTitle}>Course:</Typography>
-                        <ListItemText primary={<Typography variant="body1" color="secondary">BSIT</Typography>} />
-                      </ListItem>
+                        <ListItem className={styles.list}>
+                          <Typography variant="h6" color="secondary" className={styles.listTitle}>Course:</Typography>
+                          <ListItemText primary={<Typography variant="h6" color="secondary">BSIT</Typography>} />
+                        </ListItem>
 
-                      <ListItem className={styles.list}>
-                        <Typography variant="body1" color="secondary" className={styles.listTitle}>Year/Section/Group:</Typography>
-                        <ListItemText primary={<Typography variant="body1" color="secondary">3B-G1</Typography>} />
-                      </ListItem>
+                        <ListItem className={styles.list}>
+                          <Typography variant="h6" color="secondary" className={styles.listTitle}>Year/Section/Group:</Typography>
+                          <ListItemText primary={<Typography variant="h6" color="secondary">3B-G1</Typography>} />
+                        </ListItem>
 
-                      <ListItem className={styles.list}>
-                        <Typography variant="body1" color="secondary" className={styles.listTitle}>Email:</Typography>
-                        <ListItemText primary={<Typography variant="body1" color="secondary">jeremiah@gmail.com</Typography>} />
-                      </ListItem>
+                        <ListItem className={styles.list}>
+                          <Typography variant="h6" color="secondary" className={styles.listTitle}>Email:</Typography>
+                          <ListItemText primary={<Typography variant="h6" color="secondary">jeremiah@gmail.com</Typography>} />
+                        </ListItem>
 
-                      <ListItem className={styles.list}>
-                        <Typography variant="body1" color="secondary" className={styles.listTitle}>Contact Number:</Typography>
-                        <ListItemText primary={<Typography variant="body1" color="secondary">09163130373</Typography>} />
-                      </ListItem>
+                        <ListItem className={styles.list}>
+                          <Typography variant="h6" color="secondary" className={styles.listTitle}>Contact Number:</Typography>
+                          <ListItemText primary={<Typography variant="h6" color="secondary">09163130373</Typography>} />
+                        </ListItem>
 
-                      <ListItem className={styles.list}>
-                        <Typography variant="h6" color="secondary" className={styles.listTitle}>Payment Status:</Typography>
-                        <ListItemText primary={<Typography variant="h6" color="secondary">PAID</Typography>} />
-                      </ListItem>
+                        <ListItem className={styles.list}>
+                          <Typography variant="h6" color="secondary" className={styles.listTitle}>Payment Status:</Typography>
+                          <ListItemText primary={<Typography variant="h6" color="secondary">PAID</Typography>} />
+                        </ListItem>
 
-                    </List>
-                  </div>
+                      </List>
+                    </div>
+                  </Grid>
                 </Grid>
               </Grid>
             </Grid>
-          </Grid>
-          <div className={styles.submitButtonDiv}>
-            <RemoteSubmitButton>
-              SAVE
-            </RemoteSubmitButton>
-          </div>
-        </Paper>
+            <div className={styles.submitButtonDiv}>
+              <RemoteSubmitButton>
+              CHECK IN
+              </RemoteSubmitButton>
+            </div>
+          </Paper>
+        </Center>
       </LayoutWithTopbarAndSidebar>
     );
   }
